517c32cc79bd2347395dd8fe54f4b8ac52e27e46
Leslie Reyes <lesliereyesv@gmail.com>
2026-03-20 22:28:06 -0300

    enums

---
+++ src/main/java/modelo/enums/EstadoMantenimiento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package modelo.enums;
+
+/**
+ *
+ * @author Jonathan Fuentealba, Gustavo Gallegos, Rodolfo Guerrero, Leslie Reyes
+ */
+
+public enum EstadoMantenimiento {
+    Programado,
+    Completado,
+    Cancelado
+}
