9ed70fbbc0f9cca8bff900ee98723cc5eb13d4fa
Gustavo <contacto.gustavog@gmail.com>
2026-04-12 00:02:03 -0400

    feat: configurar JaCoCo y añadir pruebas unitarias para Usuario

--- src/test/java/modelo/UsuarioTest.java
+++ src/test/java/modelo/UsuarioTest.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
+package modelo;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,7 +13,28 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class UsuarioTest {
     
-    public UsuarioTest() {
+ @Test
+    public void testCorreoCorporativo() {
+        Usuario u = new Usuario();
+        
+        String emailReal = "carlos.mendoza@hirata.cl";
+        String nombreReal = "Carlos Mendoza";
+        
+        u.setNombreUsuario(nombreReal);
+        u.setEmail(emailReal);
+        
+        assertEquals("Carlos Mendoza", u.getNombreUsuario());
+        assertEquals("carlos.mendoza@hirata.cl", u.getEmail());
     }
+    
+ @Test
+    public void testNombreNoPuedeSerNulo() {
+        Usuario u = new Usuario();
+        // Verificamos que lance NullPointerException si el nombre es null
+        assertThrows(NullPointerException.class, () -> {
+            u.setNombreUsuario(null);
+        });
+    }
+   
     
 }
