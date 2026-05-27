c3dd00635e5e414ac475ba7d3278c61034016a41
Jonathan-Fuentealba <j.fuentealba.contacto@gmail.com>
2026-05-27 13:07:01 -0400

    Vista inventario piezas

--- src/main/java/vista/VistaLogin.java
+++ src/main/java/vista/VistaLogin.java
@@ -19,7 +19,6 @@ public class VistaLogin extends javax.swing.JFrame {
 
     public VistaLogin() {
         initComponents();
-        EstiloHirata.aplicarVentana(this, getTitle(), getWidth() > 0 ? getWidth() : 1000, getHeight() > 0 ? getHeight() : 650);
 
         this.setSize(1000, 650);
 
